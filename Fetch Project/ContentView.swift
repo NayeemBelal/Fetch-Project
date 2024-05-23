51d740a860235bdf807da6805dfccc9ef9c2c21f
NayeemBelal <nayeem.belal.1@gmail.com>
2024-05-22 22:00:07 -0500

    Initial Commit

---
+++ Fetch Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch Project
+//
+//  Created by Nayeem Belal on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("youir mom").font(.title)
+            Text("Hello, my efefwefw!")
+                .font(.title)
+                .fontWeight(.semibold)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch Project/Fetch_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_ProjectApp.swift
+//  Fetch Project
+//
+//  Created by Nayeem Belal on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
